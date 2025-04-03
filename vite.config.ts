0a664be2d70df9dd870a8c30124fe7903426738c
Soma Hargitai <hargitai.soma@gmail.com>
2025-04-03 12:17:22 +0200

    rewrite for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({}) => {
+  const isProduction = process.env.VERCEL_ENV === "production";
+  return {
+    base: isProduction ? "/toolbox/" : "/", // Use "/toolbox/" only in production
+    plugins: [react()],
+  };
+});
